a4bd47f6a2ca0cf4f1bef70c719e682ef8bd5db4
JeanExtreme002 <jeangamerextreme@gmail.com>
2023-05-20 17:46:44 -0300

    fixed SJF algorithm

--- process_scheduler/sjf.py
+++ process_scheduler/sjf.py
@@ -3,18 +3,30 @@ from process import Process
 
 
 class SJFProcessScheduler(ProcessScheduler):
+    __running = None
+
+    def remove_process(self, process: Process):
+        super().remove_process(process)
+
+        if process == self.__running:
+            self.__running = None
 
     def run(self) -> Process:
         if not self.processes: return
 
         processes = self.processes
+        process = self.__running
 
-        # Ordena os processos pela duração e obtém o mais próximo de finalizar.
-        processes.sort(key = lambda process: process.duration)
-        process = processes[0]
+        # Se não houver processo em execução, ordena os processos pela duração e obtém o mais próximo de finalizar.
+        if process is None:
+            processes.sort(key = lambda process: process.duration)
+            process = processes[0]
+
+        self.__running = process
 
         if process.is_finished():
             self.remove_process(process)
 
-        for process in processes[1:]:
-            process.wait()
+        for process_asleep in processes:
+            if process_asleep.id != process.id:
+                process_asleep.wait()
