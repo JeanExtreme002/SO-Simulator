96015bba81ae08c79d5df9236d44fca760e7bd90
JeanExtreme002 <jeangamerextreme@gmail.com>
2023-05-19 22:53:22 -0300

    implemented SJF algorithm

--- process_scheduler/sjf.py
+++ process_scheduler/sjf.py
@@ -3,5 +3,18 @@ from process import Process
 
 
 class SJFProcessScheduler(ProcessScheduler):
+
     def run(self) -> Process:
-        pass
+        if not self.processes: return
+
+        processes = self.processes
+
+        # Ordena os processos pela duração e obtém o mais próximo de finalizar.
+        processes.sort(key = lambda process: process.duration)
+        process = processes[0]
+
+        if process.is_finished():
+            self.remove_process(process)
+
+        for process in processes[1:]:
+            process.wait()
