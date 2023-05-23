91cd0c67ce9cbb264b0206875cc403799b772dfa
JeanExtreme002 <jeangamerextreme@gmail.com>
2023-05-23 11:31:11 -0300

    implemented method reserve and use of LRUMemoryManager

--- memory_paging/lru.py
+++ memory_paging/lru.py
@@ -3,16 +3,41 @@ from process import Process
 
 
 class LRUMemoryManager(MemoryManager):
+    __count = 0
+
+    def __find_least_recently_used_address(self) -> int:
+        """
+        Retorna o endereço da página menos recentemente utilizada.
+        """
+        pass
+
     def reserve(self, process: Process) -> str:
         """
         Reserva uma página para o dado processo.
 
         :returns: Retorna o endereço da página em hexadecimal.
         """
-        pass
+        if self.__count < self.ram_memory_pages:
+            real_memory_address = self.__count
+            self.__count += 1
+        else:
+            real_memory_address = self.__find_least_recently_used_address()
+
+        return super()._reserve(process, real_memory_address, 0)
 
     def use(self, process: Process, memory_page_address: str):
         """
         Utiliza uma página de memória em uma dado endereço.
         """
-        pass
+        real_memory_address = self._translate_to_real_memory_address(memory_page_address)
+
+        # Aumenta a pontuação da página de memória utilizada.
+        value = super()._use(process, real_memory_address)
+        super()._use(process, real_memory_address, value + 1)
+
+        # Diminui a pontuação das outras páginas de memória.
+        for address in range(self.ram_memory_pages):
+            if address == real_memory_address: continue
+
+            value = super()._use(process, real_memory_address)
+            super()._use(process, real_memory_address, value - 1)
